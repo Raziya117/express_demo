708269898d6d3cfa471ad1128ec683c5c0f22084
Raziya <raziyamohammadh1176@gmail.com>
2025-09-04 14:44:48 +0530

    Initial commit - added Express server files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello, Express is working!');
+});
+app.get('/about', (req, res) => {
+  res.json({ message: 'This is the about page.' });
+});
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
